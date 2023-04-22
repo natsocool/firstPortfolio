19fe44128bae51e230f56e01909f282ab7ffd909
natsocool <116443612+natsocool@users.noreply.github.com>
2023-04-22 16:42:43 +0800

    projectContent
    Coded the My Projects part

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const allSections = document.querySelector('.main-content');
 function PageTransitions(){
     //Button click active class
     //Navigation buttons lighting on and off
-    for(let i =0; i < sectBtn.length; i++){
+    for(let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '')
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn';
         })
     }
